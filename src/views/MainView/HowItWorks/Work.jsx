16dfab21892828520f485edeb2c7321875d46314
ZAIN-GITUP <zainulabdinghani15.gmailcom>
2025-10-19 19:53:51 +0500

    final cmt

--- src/views/MainView/HowItWorks/Work.jsx
+++ src/views/MainView/HowItWorks/Work.jsx
@@ -13,7 +13,7 @@ import {
 } from "react-icons/fi";
 
 import illustrationSrc from "../../../assets/images/howitwork.png"
-import EmergencyResources from "../Componenets/Responeses/responses";
+
 
 /* card data using react-icons */
 const cardData = [
@@ -304,7 +304,7 @@ export default function EmergencyResponseSliderWithReactIcons({
           </div>
         </div>
       </div>
-      <EmergencyResources/>
+  
     </section>
   );
 }
